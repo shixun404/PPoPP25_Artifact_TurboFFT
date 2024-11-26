0e74022e228d682b08ebc5849dc78e24ee7c7e62
shixun404 <shixun404@gmail.com>
2024-11-25 17:44:00 -0800

    update

--- test_VkFFT.sh
+++ test_VkFFT.sh
@@ -7,4 +7,4 @@ cmake -DVKFFT_BACKEND=1 ..
 make -j
 cp ../../test_VkFFT.py .
 python test_VkFFT.py
-cp vkFFT.pt TurboFFT/artifact_data/VkFFT_data/
+cp vkFFT.pt ../../TurboFFT/artifact_data/VkFFT_data/
